97b0372ee2bcc5a6977d18f4757c3df1e026cf54
Gzar81 <116944846+Gzar81@users.noreply.github.com>
2023-02-07 12:41:48 +0100

    comentado

--- src/app/components/tercero/tercero.component.ts
+++ src/app/components/tercero/tercero.component.ts
@@ -10,7 +10,7 @@ export class TerceroComponent {
   apellido: string = "";
   arrPersonas: any[] = [];
 
-  guardar(): void {
+  guardar(): void { //Recuerda que el flujo va lineal para abajo. 1º creamos la persona, 2º añadimos la persona, 3º limpiamos el formulario.
     let persona = {
       nombre: this.nombre,
       apellido: this.apellido
@@ -18,7 +18,7 @@ export class TerceroComponent {
 
   this.arrPersonas.push(persona);
   console.log(this.arrPersonas)
-  this.nombre = "";
+  this.nombre = "";// Al final de la función guardar() metemos las propiedades vacías para limpiar los campos del formulario.
   this.apellido = "";
   }
 
